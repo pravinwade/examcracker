4bddbdbcb5e594389ad9308c1d4663283185f0ac
pravinwade <pravin11wade@gmail.com>
2019-03-16 10:13:15 +0530

    Initial

---
+++ ExamCracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamCracker.Startup))]
+namespace ExamCracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
